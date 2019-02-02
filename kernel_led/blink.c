925b923a2477b2ed000210fc17e10ac5025b5a14
rogerkupari <roger.kupari@hotmail.com>
2019-02-02 15:44:29 +0000

    valmis

--- kernel_led/blink.c
+++ kernel_led/blink.c
@@ -2,25 +2,31 @@
 #include <linux/module.h>
 #include <linux/gpio.h>
 
+// kernel_led/blink.c
 
 
-
-static int blink_init(void)
+static int __init blink_init(void)
 {
-	int tila = gpio_direction_output(252, 0);
-	printk(KERN_ALERT "gpio3 is on %d \n",tila);
+
+	// pinnin ohjaus, "48" -> High
+	gpio_direction_output(48, 1);
+	// Tama teksti tulee nakyviin dmesgiin ja funktiolla ilmoitetaan ko. pinnin tila (1/0)
+	printk(KERN_ALERT "Moduuli asennettu, ledin ohjaus on tilassa: %d \n",gpio_get_value(48));
 	return 0;
 }
 
-static void blink_exit(void)
+static void __exit blink_exit(void)
 {
-	int tila = gpio_direction_output(252, 1);
-	printk(KERN_ALERT "gpio3 is off, module is gone %d\n", tila);
-	
+	gpio_direction_output(48, 0);
+	printk(KERN_ALERT "Moduuli poistettu, ledin ohjaus on tilassa: %d\n", gpio_get_value(48));
+
 }
 
+// Moduulin asennushetken funktio
 module_init(blink_init);
-
+// poistohetken funktio
 module_exit(blink_exit);
-
 MODULE_LICENSE("GPL");
+MODULE_AUTHOR("ROGER KUPARI");
+MODULE_VERSION("1");
+
